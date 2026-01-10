aca0e6e7c22b24db3766a0c3c09e6976f992f968
Tolga745 <tolgatek745@gmail.com>
2026-01-11 00:24:04 +0300

    dataview fix

--- contree/code/DataStructures/include/gpu_dataview.h
+++ contree/code/DataStructures/include/gpu_dataview.h
@@ -3,6 +3,16 @@
 
 #include "gpu_structs.h"
 
+#ifdef __CUDACC__
+// In CUDA files (.cu), we have access to the runtime headers
+#include <cuda_runtime.h>
+#else
+// In C++ files (.cpp), we forward declare to avoid including huge CUDA headers
+// cudaStream_t is a pointer to an opaque struct CUstream_st
+struct CUstream_st;
+typedef struct CUstream_st *cudaStream_t;
+#endif
+
 void split_gpu_dataview(const GPUDataview& parent, GPUDataview& left, GPUDataview& right, int split_feat_idx, float threshold, cudaStream_t stream = 0);
 
 #endif
