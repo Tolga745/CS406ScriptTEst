18d10e914d059768bc096a3178d92b39f0ab45ee
Tolga745 <tolgatek745@gmail.com>
2026-01-11 00:15:25 +0300

    dataview issue fix

--- contree/code/DataStructures/include/gpu_dataview.h
+++ contree/code/DataStructures/include/gpu_dataview.h
@@ -1,8 +1,7 @@
 #ifndef GPU_DATAVIEW_H
 #define GPU_DATAVIEW_H
 
-#include \"gpu_structs.h\"
-#include <cuda_runtime.h>
+#include "gpu_structs.h"
 
 void split_gpu_dataview(const GPUDataview& parent, GPUDataview& left, GPUDataview& right, int split_feat_idx, float threshold, cudaStream_t stream = 0);
 
